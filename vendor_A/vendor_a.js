41deb8f2252cd1d97b3c72880ff587d4af4be9ff
Yogimaulanaaa <maulanayogi938@gmail.com>
2025-12-19 18:06:51 +0700

    Add Vendor A spec files

--- vendor_A/vendor_a.js
+++ vendor_A/vendor_a.js
@@ -1,12 +1,19 @@
 // Simulasi data Vendor A (Warung Legacy) - semua tipe data string
-const dataVendorA = [
-    {
-        "kd_produk": "A001",
-        "nm_brg": "Kopi Bubuk 100g",
-        "hrg": "15000",
-        "ket_stok": "ada"
-    }
-];
+const path = require('path');
 
-// Output JSON
+let dataVendorA;
+try {
+    dataVendorA = require('./specifikasi_data.json');
+} catch (err) {
+    dataVendorA = [
+        {
+            "kd_produk": "A001",
+            "nm_brg": "Kopi Bubuk 100g",
+            "hrg": "15000",
+            "ket_stok": "ada"
+        }
+    ];
+}
+
+// Output JSON (stringified)
 console.log(JSON.stringify(dataVendorA, null, 4));
